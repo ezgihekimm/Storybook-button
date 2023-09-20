246a13a9480589919f726a8c2d370220611dca33
ezgi.hekim.2000 <ezgi.hekim.2000@gmail.com>
2023-09-21 00:14:59 +0300

    story button

---
+++ src/stories/assets/exampleIcon.tsx
@@ -0,0 +1,9 @@
+const SvgComponent = ({ classNames }: { classNames?: string }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 13.14 12.83">
+    <path
+      className={classNames}
+      d="M.64 0a.51.51 0 0 0-.5.13.51.51 0 0 0-.14.52l1.64 5.27h5.68a.5.5 0 0 1 .5.5.5.5 0 0 1-.5.5H1.64L0 12.19a.49.49 0 0 0 .12.49.47.47 0 0 0 .5.13 40.18 40.18 0 0 0 12.3-6 .48.48 0 0 0 .2-.4.5.5 0 0 0-.2-.41A40.41 40.41 0 0 0 .64 0Z"
+    />
+  </svg>
+)
+export default SvgComponent
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,14 +5,23 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/stories/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        archivo: ['var(--font-archivo)'],
       },
+      colors: {
+        'primary-blue-50': '#E2EDFE',
+        'primary-blue-100': '#B3D1FE',
+        'primary-blue-500': '#06F',
+        'primary-blue-700': '#0056D4',
+        'primary-dark-blue-500': '#25245C',
+        'light-theme-300': '#F7F7F8',
+        'light-theme-400': '#E7E9EE',
+        'light-theme-600': '#A4AEC0',
+      }
     },
   },
   plugins: [],
